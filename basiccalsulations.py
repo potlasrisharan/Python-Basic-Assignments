17015aea2e22bc557b110d595419256d8d87ee98
potlasrisharan <potlasrisharanchowdary@gmail.com>
2026-01-31 23:30:12 +0530

    Initial commit with Python tasks.

---
+++ basiccalsulations.py
@@ -0,0 +1,20 @@
+"""
+Problem Statement: Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen.
+"""
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+Addition=num1+num2
+Subtraction=num1-num2
+Multiplication=num1*num2
+Division=num1/num2
+print("The Addition of two numbers is :",Addition)
+print("The Subtraction of two numbers is :",Subtraction)
+print("The Multiplication of two numbers is :",Multiplication)
+print("The Division of two numbers is :",Division)
