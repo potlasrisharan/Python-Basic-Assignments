5614e9b8859286b21845cefc43eb782b50adf32f
potlasrisharan <potlasrisharanchowdary@gmail.com>
2026-02-08 13:03:37 +0530

    Assignment 2 files

---
+++ Assignment-2/Even_odd.py
@@ -0,0 +1,7 @@
+n=int(input("Enter a number :"))
+if n%2==0:
+    print(f"{n} is a even number")
+elif n%2!=0:
+    print(f"{n} is odd number")
+else :
+    print("enter a valid number ")
---
+++ Assignment-2/sum_of_first_50.py
@@ -0,0 +1,4 @@
+total_sum=0
+for i in range(1,51):
+    total_sum+=i
+print(f"sum of numbers from 1 - 50 is {total_sum}")
